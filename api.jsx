308fc72fbb8147ad5187b5808918d2fbdda57c44
Jayesh797 <jayeshlandge797@gmail.com>
2024-03-12 20:50:06 +0530

    Added api.jsx file

---
+++ api.jsx
@@ -0,0 +1,17 @@
+export const questions=[
+    {
+        id:1,
+        question:'Introduction',
+        answer:'Greetings! My name is Jayesh, and I am a dedicated student currently embarking on my Bachelor of Technology degree at Ramdeobaba College of Engineering and Management. With a passion for learning and a drive for excellence, I am fully immersed in the dynamic world of technology and engineering.'
+    },
+    {
+        id:2,
+        question:'Education',
+        answer:'I completed my schooling at Sanjuba High School and my Higher Secondary Certificate (HSC) at Shivaji Science College. Currently, I am pursuing a Bachelor of Technology degree at Shri Ramdeobaba College of Engineering and Management'
+    },
+    {
+        id:3,
+        question:'My Intrests and skills',
+        answer:'My interests and skills encompass a dynamic blend of web development and emerging technologies like machine learning, deep learning, and AI. Within the realm of web development, I have a basic knowledge of essential tools such as Node.js, Express, MongoDB, and React, allowing me to create engaging and user-friendly digital experiences'
+    }
+]
