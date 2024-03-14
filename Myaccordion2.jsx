e0012c5c4256de8d94a0baffc36a2ba4a933d534
Jayesh797 <jayeshlandge797@gmail.com>
2024-03-14 20:49:46 +0530

    updated Myaccordion2.jsx

--- Myaccordion2.jsx
+++ Myaccordion2.jsx
@@ -7,10 +7,10 @@ const Myaccordion2=({question,answer})=>
     const [icon,setIcon]=useState(false)
     return(
         <>
-        <div>
+        <div className="main">
             <div className="sub_div">
                 <p  onClick={()=>setIcon(!icon)}>{icon?<RemoveCircleIcon/>:<AddCircleIcon/>}</p>
-                <h1>{question}</h1>
+                <h1 className="question_class">{question}</h1>
             </div>
             {icon && <p className="answer" >{answer}</p>}
         </div>
