f42e835adfbedcb1cb6b3af5c67dafebab40e96e
Jayesh797 <jayeshlandge797@gmail.com>
2024-03-15 17:14:50 +0530

    api.jsx

--- api.jsx
+++ api.jsx
@@ -1,17 +1,49 @@
-export const questions=[
+export const questions = [
     {
-        id:1,
-        question:'Introduction',
-        answer:'Greetings! My name is Jayesh, and I am a dedicated student currently embarking on my Bachelor of Technology degree at Ramdeobaba College of Engineering and Management. With a passion for learning and a drive for excellence, I am fully immersed in the dynamic world of technology and engineering.'
+        id: 1,
+        question: 'Introduction',
+        answer: 'Greetings! My name is Jayesh, and I am a dedicated student currently embarking on my Bachelor of Technology degree. With a passion for learning and a drive for excellence, I am fully immersed in the dynamic world of technology and engineering.'
     },
     {
-        id:2,
-        question:'Education',
-        answer:'I completed my schooling at Sanjuba High School and my Higher Secondary Certificate (HSC) at Shivaji Science College. Currently, I am pursuing a Bachelor of Technology degree at Shri Ramdeobaba College of Engineering and Management'
+        id: 2,
+        question: 'Education',
+        answer: 'I completed my schooling at Sanjuba High School and my Higher Secondary Certificate (HSC) at Shivaji Science College. Currently, I am pursuing a Bachelor of Technology degree at Shri Ramdeobaba College of Engineering and Management'
     },
     {
-        id:3,
-        question:'My Intrests and skills',
-        answer:'My interests and skills encompass a dynamic blend of web development and emerging technologies like machine learning, deep learning, and AI. Within the realm of web development, I have a basic knowledge of essential tools such as Node.js, Express, MongoDB, and React, allowing me to create engaging and user-friendly digital experiences'
+        id: 3,
+        question: 'Skills',
+        answer: 'My skills encompass a blend of web development and emerging technologies like machine learning, deep learning, and AI. Within the realm of web development, I have a basic knowledge of essential tools such as Node.js, Express, MongoDB, and React.'
+    },
+    {
+        id: 4,
+        question: 'My Certifications',
+        answer: (
+            <div>
+                <p>Machine Learning with Python <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Coursera-Logo_600x600.svg/2048px-Coursera-Logo_600x600.svg.png" alt="Description" width="25px" height="25px"/></p>
+                <p>Verify at: <a href="https://www.coursera.org/account/accomplishments/verify/8F45RGXEDJKM" target="_blank" rel="noopener noreferrer">Check Out</a></p>
+                <p>Python for DS, AI Development <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Coursera-Logo_600x600.svg/2048px-Coursera-Logo_600x600.svg.png" alt="Description" width="25px" height="25px"/></p>
+                <p>Verify at: <a href="https://www.coursera.org/account/accomplishments/verify/NNKSMC7JGL9A" target="_blank" rel="noopener noreferrer">Check Out</a></p>
+            </div>
+        )
+    },
+    {
+        id: 5,
+        question: 'Intrests and hobbies',
+        answer: `I'm passionate about Indian history and prefer learning from various sources, including articles, documentaries, and online resources, instead of just books. This multi-source approach broadens my understanding and enriches my knowledge comprehensively.In addition to my passion for Indian history, I enjoy drawing as a hobby.`
+    },
+    {
+        id: 6,
+        question: 'References',
+        answer: (
+            <div>
+                <p>Check Out my LinkedIn Profile:</p>
+                <a href="https://www.linkedin.com/in/jayesh-landge-b78911271/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png" alt="Description of the image" width="100px" height="25px" />
+                </a>
+            </div>
+
+
+        )
     }
 ]
+
